3c422e67deee4f63fd40c214fc242c3d8c241c7c
shch989 <tankpop11@gmail.com>
2023-02-24 22:27:22 +0900

    pipe를 사용하여 요청 데이터 검사

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
@@ -21,9 +23,10 @@ export class BoardsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
-  }
+  } 
 
   @Get('/:id')
   getBoardById(@Param('id') id: string): Board {
