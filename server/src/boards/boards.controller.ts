b965a6155026844e37871ec237a2661938b317a6
shch989 <tankpop11@gmail.com>
2023-02-24 20:10:27 +0900

    nest get 요청

---
+++ server/src/boards/boards.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard():Board[] {
+    return this.boardsService.getAllBoards()
+  }
+
+}
