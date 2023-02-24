4c589a582a1e904783025f5e8b2acfee35385379
shch989 <tankpop11@gmail.com>
2023-02-24 20:45:45 +0900

    Post 요청 DTO를 사용하여 구현

--- server/src/boards/boards.controller.ts
+++ server/src/boards/boards.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get()
-  getAllBoard():Board[] {
-    return this.boardsService.getAllBoards()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
   }
 
+  @Post()
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto
+  ): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
 }
